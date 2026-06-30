217c0509fa4fba6005e506a17ebd3fac3653ab1e
Sabarivasan S <s.sabarivasan2007@gmail.com>
2026-06-30 20:24:33 +0530

    Delete E-Commerce directory

--- E-Commerce/script.js
+++
@@ -1,9 +0,0 @@
-var sideNavbar = document.querySelector(".side-navbar");
-var menu = document.getElementsByClassName("navbar-menu-toggle")[0];
-var close=document.getElementsByClassName("fa-xmark")[0];
-menu.addEventListener("click", function() {
-    sideNavbar.style.left = "0";
-});
-close.addEventListener("click", function() {
-    sideNavbar.style.left = "-100%";
-})
